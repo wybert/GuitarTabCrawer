ac86ba07a832fb5c5af084367e82674f4a3632e2
wybert <1072534112@qq.com>
2019-10-24 19:39:33 +0800

    searh tabs

--- guitar_dl.py
+++ guitar_dl.py
@@ -6,6 +6,9 @@ from urllib.request import urlretrieve
 import os
 import img2pdf
 import click
+import requests
+from urllib.parse import quote
+
 
 
 def get_soup(url):  
@@ -59,8 +62,8 @@ def save_images(images_list,tab_id):
 
 
 
-@click.command()
-@click.option('--url', help='the url of one guitar tab')
+# @click.command()
+# @click.option('--url', help='the url of one guitar tab')
 def guitar_dl(url):
 
     """A tools for download tabs from jitashe.org """
@@ -70,7 +73,7 @@ def guitar_dl(url):
 
     soup = get_soup(url)
     images_list = find_image_url_and_name(soup)
-    tab_name = images_list[0]["name"][:-6]
+    tab_name = images_list[0]["name"].replace(":","_")[:-6]
 
 
     print("Downloading %s "%tab_name)
@@ -79,11 +82,92 @@ def guitar_dl(url):
 
     print("*"*60)
     print("Converting to pdf...")
+    print(file_path_name_list)
     a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
     layout_fun = img2pdf.get_layout_fun(a4inpt)
+    print(tab_name)
     with open(tab_name + ".pdf", "wb") as f:
         f.write(img2pdf.convert(file_path_name_list, layout_fun=layout_fun))
     print("Done!")
 
+
+
+def parse_tab(tab_div):
+
+    result = {}
+    title = tab_div.find("a",{"class":"title"})
+    url = title["href"]
+    text = title.text
+
+    tag_list = tab_div.find("div",{"class":"taglist"})
+    tags = tag_list.text
+
+    result["title"] = text
+    result["url"] = "https://www.jitashe.org" + url
+    result["tags"] = tags
+
+    return result
+
+def find_search_result(soup):
+    
+    div = soup.find("div",{"class":"tab-list"})
+    # print(div)
+    tab_divs = div.find_all("div",{"class":"tab-item"})
+
+    result_list = []
+    for tab_div in tab_divs:
+        result = parse_tab(tab_div)
+        result_list.append(result)
+
+    return result_list
+
+def search_tabs(search_string):
+    
+    url_base = "https://www.jitashe.org/search/tab/%s/"
+
+    # param = {"wd": "莫烦Python"}  # 搜索的信息
+    # r = requests.get('http://www.baidu.com/s', params=param)
+    # print(r.url)
+    url = url_base%quote(search_string)
+    soup = get_soup(url)
+    # print(soup)
+
+    result_list = find_search_result(soup)
+    result_list = [item for item in result_list if "图片谱" in item["tags"]]
+    for i,item in enumerate(result_list):
+        print("-"*60)
+       
+        print(i,item["url"],item["title"],item["tags"].replace("\n"," "))
+
+    # print(result_list)
+
 if __name__ == '__main__':
-    guitar_dl()
+    # guitar_dl()
+
+
+
+    urls = [
+    # "https://www.jitashe.org/tab/14626/",
+    # "https://www.jitashe.org/tab/1299551/",
+    "https://www.jitashe.org/tab/14435/",
+    # "https://www.jitashe.org/tab/1340565/",
+    # "https://www.jitashe.org/tab/1307674/",
+    # "https://www.jitashe.org/tab/9933/",
+    # "https://www.jitashe.org/tab/9132/",
+    # "https://www.jitashe.org/tab/67507/",
+    # "https://www.jitashe.org/tab/1335746/",
+    # "https://www.jitashe.org/tab/9851/",
+    # "https://www.jitashe.org/tab/93549/"
+    ]
+
+    # for url in urls:
+    #     print(url)
+    #     guitar_dl(url)
+    search_tabs("女儿情")
+# ------------------------------
+
+# download 有谱么
+
+
+
+
